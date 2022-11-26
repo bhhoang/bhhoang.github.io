6db2b54437561fdd5103d1540459cd44b859fba3
bhhoang <canhchimcuajin@gmail.com>
2022-11-26 20:21:48 +0700

    oop

--- docs/.vuepress/.temp/pages/Lectures/index.html.js
+++ docs/.vuepress/.temp/pages/Lectures/index.html.js
@@ -1,14 +1,1 @@
-export const data = JSON.parse("{\"key\":\"v-403d2921\",\"path\":\"/Lectures/\",\"title\":\"Menu\",\"lang\":\"en-US\",\"frontmatter\":{},\"excerpt\":\"\",\"headers\":[{\"level\":2,\"title\":\"Philosophy\",\"slug\":\"philosophy\",\"link\":\"#philosophy\",\"children\":[]}],\"git\":{},\"filePathRelative\":\"Lectures/README.md\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
+export const data = JSON.parse("{\"key\":\"v-403d2921\",\"path\":\"/Lectures/\",\"title\":\"Menu\",\"lang\":\"en-US\",\"frontmatter\":{},\"excerpt\":\"\",\"headers\":[{\"level\":2,\"title\":\"Philosophy\",\"slug\":\"philosophy\",\"link\":\"#philosophy\",\"children\":[]}],\"git\":{\"updatedTime\":1669466710000,\"contributors\":[{\"name\":\"bhhoang\",\"email\":\"canhchimcuajin@gmail.com\",\"commits\":1}]},\"filePathRelative\":\"Lectures/README.md\"}")
--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,14 +1,1 @@
-export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"Welcome to this site\",\"lang\":\"en-US\",\"frontmatter\":{},\"excerpt\":\"\",\"headers\":[{\"level\":2,\"title\":\"Introduction\",\"slug\":\"introduction\",\"link\":\"#introduction\",\"children\":[]}],\"git\":{},\"filePathRelative\":\"README.md\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
+export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"Welcome to this site\",\"lang\":\"en-US\",\"frontmatter\":{},\"excerpt\":\"\",\"headers\":[{\"level\":2,\"title\":\"Introduction\",\"slug\":\"introduction\",\"link\":\"#introduction\",\"children\":[]}],\"git\":{\"updatedTime\":1669466710000,\"contributors\":[{\"name\":\"bhhoang\",\"email\":\"canhchimcuajin@gmail.com\",\"commits\":1}]},\"filePathRelative\":\"README.md\"}")
